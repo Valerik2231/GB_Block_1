f4e18f5d920f9eb47bce7b62099decccf7aca984
valerik2231@yandex.ru <valerik2231@yandex.ru>
2023-12-08 13:49:20 +0300

    Добавлен код на C#

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Runtime.Intrinsics.X86;
+using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] input = {"Hello", "2", "World", ":-)" };
+        int count = 0;
+        int resultcount = 0;
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i].Length <=3)
+            {
+                count++;
+            }
+        }
+        var result = new string[count];
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input[i].Length <= 3)
+            {
+                result[resultcount] = input[i];
+                resultcount++;
+            }
+        }
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    
+
+
+
+
+
+}
+
+
+
+
+
